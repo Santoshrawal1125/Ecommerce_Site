81dc56f80afa4bcc7845d1267dd4bf5cffa18da0
Santosh Rawal <awesomekid.spa@gmail.com>
2023-06-28 16:57:08 +0545

    MODELS

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
+admin.site.register(Category)
+admin.site.register(Slider)
+admin.site.register(Ad)
+admin.site.register(Brand)
+admin.site.register(Product)
 # Register your models here.
+
