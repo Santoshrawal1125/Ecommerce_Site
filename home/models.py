0ac0abe90cb73f9b67c2e03dc35e7d60bfc32e37
Santosh Rawal <awesomekid.spa@gmail.com>
2023-07-10 12:39:02 +0545

    all okay search

--- home/models.py
+++ home/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 # Create your models here.
-
 class Category(models.Model):
     name = models.CharField(max_length=200)
     logo = models.CharField(max_length=200, blank=True)
@@ -17,7 +16,7 @@ class Slider(models.Model):
     image = models.ImageField(upload_to='media')
     description = models.TextField(blank=True)
     rank = models.IntegerField()
-    url = models.URLField(blank=True, max_length=500)
+    ulr = models.URLField(blank=True, max_length=500)
 
     def __str__(self):
         return self.name
@@ -37,6 +36,7 @@ class Brand(models.Model):
     name = models.CharField(max_length=500)
     image = models.ImageField(upload_to='media')
     rank = models.IntegerField()
+    slug = models.CharField(max_length=500, default="")
 
     def __str__(self):
         return self.name
@@ -47,29 +47,27 @@ LABELS = (('', 'default'), ('new', 'new'), ('sale', 'sale'), ('hot', 'hot'))
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=500)
+    name = models.CharField(max_length=300)
     price = models.IntegerField()
     discounted_price = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    description = models.TextField(blank=True)
-    specification = models.TextField(blank=True)
     image = models.ImageField(upload_to='media')
-    rank = models.IntegerField()
+    desription = models.TextField(blank=True)
+    specification = models.TextField(blank=True)
     stock = models.CharField(choices=STOCK, max_length=50)
-    labels = models.CharField(choices=LABELS, max_length=50)
+    labels = models.CharField(choices=LABELS, max_length=50, blank=True)
+    slug = models.CharField(max_length=500, default="")
 
     def __str__(self):
         return self.name
 
 
 class CustomerReview(models.Model):
-    name = models.CharField(max_length=500)
-    post = models.CharField(max_length=200)
-    comment = models.TextField()
+    name = models.CharField(max_length=300)
+    post = models.CharField(max_length=300)
+    comment = models.TextField(blank=True)
     image = models.ImageField(upload_to='media')
 
     def __str__(self):
         return self.name
-
-
--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('category/<slug>', CategoryView.as_view(), name='category'),
+    path('brand/<slug>', BrandView.as_view(), name='brand'),
+    path('product/<slug>', ProductDetail.as_view(), name='product'),
+    path('search', SearchView.as_view(), name='search'),
 ]
